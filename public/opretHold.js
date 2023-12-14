f896cae9c35ded3dda2f81da4e46cfa7c3209d49
alla2297 <alla2297@edu.easj.dk>
2023-12-14 14:43:34 +0100

    desing and you kan add days

--- public/opretHold.js
+++ public/opretHold.js
@@ -48,8 +48,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const selectSport = document.getElementById("Sport");
     const selectBaneType = document.getElementById("BaneType");
     const addBaneButton = document.getElementById('addBane');
+    const addTimeButton = document.getElementById('addTime');
     // Listen for changes in the 'Sport' select element
     selectSport.addEventListener("change", function() {
+        const selectbaner = document.getElementById('listBaner');
+        selectbaner.innerHTML="";
         populateSelectOptions(baneData);
     });
 
@@ -57,21 +60,55 @@ document.addEventListener('DOMContentLoaded', function() {
         populateSelectOptions(baneData);
     });
     //add baner
-    addBaneButton.addEventListener('click', function() {
+    addBaneButton.addEventListener('click', function(event) {
         event.preventDefault(); // Prevents the default form submission behavior
         const selectElement = document.getElementById('baner');
         const selectedBaneName = selectElement.value;
         // Check if a bane has been selected
         if (selectedBaneName && selectedBaneName !== 'Vælg') {
-            console.log(selectedBaneName); // Display the selected bane name
-            // Perform further actions with the selectedBaneName as needed
-
+            const selectbaner = document.getElementById('listBaner');
+            const selectedBaneName = selectElement.value;
+            
+            // Check if an element with the same value already exists in the list
+            const existingElements = selectbaner.querySelectorAll(`li[value="${selectedBaneName}"]`);
+            if (existingElements.length === 0) {
+                // If no element with the same value exists, add the new one
+                selectbaner.innerHTML += `<li class="chooseBane" value="${selectedBaneName}">${selectElement.options[selectElement.selectedIndex].textContent}</li>`;
+            } else {
+                console.log('Element already exists in the list.');
+                // Handle the case where the element already exists (e.g., display an error message)
+            }
 
         } else {
             console.log('Please select a valid bane.'); // Show a message if no bane is selected
         }
     });
     //    Shift+Alt+A Toggle block comment
+
+    //add time
+    addTimeButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevents the default form submission behavior
+        const selectElement = document.getElementById('time');
+        const selectedTime = selectElement.value;
+        //listTimes
+
+        if (selectedTime && selectedTime !== 'Vælg') {
+            const selectTimes = document.getElementById('listTimes');
+            const existingElements = selectTimes.querySelectorAll(`li[value="${selectedTime}"]`);
+
+            if (existingElements.length === 0) {
+                // If no element with the same value exists, add the new one
+                selectTimes.innerHTML += `<li class="chooseBane" value="${selectedTime}">${selectElement.options[selectElement.selectedIndex].textContent}</li>`;
+            } else {
+                console.log('Element already exists in the list.');
+                // Handle the case where the element already exists (e.g., display an error message)
+            }
+
+        } else {
+            console.log('Please select a valid time.'); // Show a message if no bane is selected
+        }
+
+    });
 
     //input todays dato in input tag
     let listOfToday = ["date", "PeriodeStartDate", "PeriodeEndDate"];
